9a1cfed46bc10fb13d9087db91d9f3d9489d81a0
Megan Bedell <megbedell@users.noreply.github.com>
2019-05-09 10:42:56 -0400

    Revert "Speeding up interp for 1D inputs"

--- wobble/interp/interp_op.cc
+++ wobble/interp/interp_op.cc
@@ -23,9 +23,11 @@ REGISTER_OP("Interp")
     TF_RETURN_IF_ERROR(c->WithRankAtLeast(c->input(1), 1, &x));
     TF_RETURN_IF_ERROR(c->WithRankAtLeast(c->input(2), 1, &y));
 
-    // The final dimension of x and y must match
-    shape_inference::DimensionHandle dx = c->Dim(x, -1), dy = c->Dim(y, -1);
-    TF_RETURN_IF_ERROR(c->Merge(dx, dy, &dx));
+    TF_RETURN_IF_ERROR(c->Merge(x, y, &x));
+
+    TF_RETURN_IF_ERROR(c->Subshape(t, 0, -1, &t));
+    TF_RETURN_IF_ERROR(c->Subshape(x, 0, -1, &x));
+    TF_RETURN_IF_ERROR(c->Merge(t, x, &t));
 
     c->set_output(0, c->input(0));
     c->set_output(1, c->input(0));
@@ -38,7 +40,7 @@ class InterpOpBase : public OpKernel {
   explicit InterpOpBase (OpKernelConstruction* context) : OpKernel(context) {}
 
   virtual void DoCompute (OpKernelContext* context,
-    bool x_one_d, bool y_one_d, int64 size,
+    int64 size,
     int M, const T* const x, const T* const y,
     int N, const T* const t, T* v, int* inds) = 0;
 
@@ -52,32 +54,23 @@ class InterpOpBase : public OpKernel {
     int64 ndim = t_tensor.dims();
     OP_REQUIRES(context, ndim >= 1,
         errors::InvalidArgument("t must be at least 1D"));
-
-    bool x_one_d = x_tensor.dims() == 1;
-    bool y_one_d = y_tensor.dims() == 1;
-    OP_REQUIRES(context, (x_tensor.dims() == ndim) || x_one_d,
-        errors::InvalidArgument("x and t must have the same number of dimensions or x must be 1D"));
-    OP_REQUIRES(context, (y_tensor.dims() == ndim) || y_one_d,
-        errors::InvalidArgument("y and t must have the same number of dimensions or y must be 1D"));
-
-    bool outer = y_tensor.dim_size(y_tensor.dims() - 1) == x_tensor.dim_size(x_tensor.dims() - 1);
-    OP_REQUIRES(context, ((x_one_d || y_one_d) && outer) || (x_tensor.shape() == y_tensor.shape()),
-        errors::InvalidArgument("x and y must be the same outer dimension"));
+    OP_REQUIRES(context, x_tensor.dims() == ndim,
+        errors::InvalidArgument("x and t must have the same number of dimensions"));
+    OP_REQUIRES(context, y_tensor.shape() == x_tensor.shape(),
+        errors::InvalidArgument("x and y must be the same shape"));
 
     // Compute the full size of the inner dimensions
     int64 size = 1;
     for (int64 k = 0; k < ndim - 1; ++k) {
       int64 dim = t_tensor.dim_size(k);
       size *= dim;
-      OP_REQUIRES(context, x_one_d || (x_tensor.dim_size(k) == dim),
-          errors::InvalidArgument("incompatible dimensions"));
-      OP_REQUIRES(context, y_one_d || (y_tensor.dim_size(k) == dim),
+      OP_REQUIRES(context, x_tensor.dim_size(k) == dim,
           errors::InvalidArgument("incompatible dimensions"));
     }
 
     // The outer dimensions
     const int64 N = t_tensor.dim_size(ndim - 1);
-    const int64 M = x_tensor.dim_size(x_tensor.dims() - 1);
+    const int64 M = x_tensor.dim_size(ndim - 1);
     OP_REQUIRES(context, N <= tensorflow::kint32max,
         errors::InvalidArgument("too many elements in tensor"));
     OP_REQUIRES(context, M <= tensorflow::kint32max,
@@ -97,12 +90,14 @@ class InterpOpBase : public OpKernel {
     auto inds    = inds_tensor->flat_inner_dims<int, 2>();
 
     DoCompute(context,
-      x_one_d, y_one_d, size,
+      size,
       M, x.data(), y.data(),
       N, t.data(), v.data(), inds.data()
     );
   }
 };
+
+
 
 template <class Device, typename T>
 class InterpOp;
@@ -114,7 +109,7 @@ class InterpOp<CPUDevice, T> : public InterpOpBase<T> {
     explicit InterpOp (OpKernelConstruction* context) : InterpOpBase<T>(context) {}
 
     void DoCompute (OpKernelContext* ctx,
-      bool x_one_d, bool y_one_d, int64 size,
+      int64 size,
       int M, const T* const x, const T* const y,
       int N, const T* const t, T* v, int* inds
     ) override {
@@ -123,9 +118,7 @@ class InterpOp<CPUDevice, T> : public InterpOpBase<T> {
         for (int i = begin; i < end; ++i) {
           int k = i / N;
           int off_m = k * M;
-          int off_x = (x_one_d) ? 0 : off_m;
-          int off_y = (y_one_d) ? 0 : off_m;
-          inds[i] = interp::interp1d(M, x + off_x, y + off_y, t[i], v + i);
+          inds[i] = interp::interp1d(M, x + off_m, y + off_m, t[i], v + i);
         }
       };
 
@@ -135,6 +128,7 @@ class InterpOp<CPUDevice, T> : public InterpOpBase<T> {
     }
 
 };
+
 
 #define REGISTER_CPU(type)                                                 \
   REGISTER_KERNEL_BUILDER(                                                 \
